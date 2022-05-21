c6f5a1a2d6885950900ecced8983c508d3f6ed21
marin-jovanovic <marin.jovanovic12@gmail.com>
2022-05-21 21:09:03 +0200

    :rocket: integrate listings

--- src_backend/PetCareAppMVC/Features/Home/HomeController.cs
+++ src_backend/PetCareAppMVC/Features/Home/HomeController.cs
@@ -1,12 +1,68 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using AutoMapper;
+using PetCareAppMVC.Features.People;
+using MediatR;
+
 namespace ProjectMVC.Features.Home
 {
   public class HomeController : Controller
   {
-    public IActionResult Index()
+
+        //private readonly IMapper mapper;
+        //private readonly IMediator mediator;
+
+        //public HomeController(IMapper mapper, IMediator mediator)
+        //{
+        //    this.mapper = mapper;
+        //    this.mediator = mediator;
+        //}
+
+        public IActionResult Index()
     {
-      return View();
+
+            //try
+            //{
+        
+            //        var command = AutoMapper.Map<DomainServices.People.Commands.AddPersonCommand>(new Domain.People.Person(
+            //            23,
+            //            "ime",
+            //            "usrname",
+            //            "mail",
+            //            "zadnje ime",
+            //            "pass",
+            //            "mob",
+            //            "sesija123"
+            //            ));
+
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine(e);
+            //}
+
+            //    var     model = mapper.Map<DomainServices.People.Commands.AddPersonCommand>(new Domain.People.Person(
+            //            23,
+            //            "ime",
+            //            "usrname",
+            //            "mail",
+            //            "zadnje ime",
+            //            "pass",
+            //            "mob",
+            //            "sesija123"
+            //            ));
+
+            ////inace ide await
+            //mediator.Send(model);
+
+            //Console.WriteLine("model", model);
+
+
+            return View();
+
+
+
+
         }
 
 
